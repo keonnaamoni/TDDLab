79811fc7a36ed84e9fa54ff64b0d9b34cf8131a3
keonnaamoni <keonnaevans@gmail.com>
2015-04-14 11:35:06 -0400

    Added public class Test1

--- TDDTest/TDDTEST.cs
+++ TDDTest/TDDTEST.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace TDDTest
 {
+    public class TestConstanants
+    {
+         [Fact]
+        public void Test1()
+        {
+
+        }
+    }
     
+   
     
     
 }
