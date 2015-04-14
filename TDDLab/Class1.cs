2794eb77e01776d28a1f3fefe21b80fe7dfb0fcd
keonnaamoni <keonnaevans@gmail.com>
2015-04-14 13:44:25 -0400

    Created, Ran Test case for returning only consonants.
    Test Failed

--- TDDLab/Class1.cs
+++ TDDLab/Class1.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace TDDLab
 {
-    
+    public class StringInput {
+        private string p;
+
+        public StringInput(string p)
+        {
+            // TODO: Complete member initialization
+            this.p = p;
+        }
+
+        public object pullConsonants()
+        {
+            throw new NotImplementedException();
+        }
+    }
     
     
 }
